604e34eeb76cfec6360181706596b1039284707a
Ravi Teja Natchireddi <ravi@authorcafe.com>
2022-07-29 13:13:09 +0530

    Failing Build

--- src/App.test.js
+++ src/App.test.js
@@ -4,5 +4,5 @@ import App from "./App";
 test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
-  expect(linkElementsa).toBeInTheDocument();
+  expect(linkElements).toBeInTheDocument();
 });
