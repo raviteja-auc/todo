33f8e0966506c92b9a22d38d96cb59d891b547f1
Ravi Teja Natchireddi <ravi@authorcafe.com>
2022-09-27 17:49:11 +0530

    test code

--- committerName.sh
+++ committerName.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-commit_id = $(cat commit.txt)
-committername = $(git show -s --format='%cn' $commit_id)
+commit_id=$(cat commit.txt)
+committername=$(git show -s --format='%cn' $commit_id)
 
 echo $committername
