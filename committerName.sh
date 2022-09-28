cf23b63e74f31909e6664c76e9117a00dc68a219
Ravi Teja Natchireddi <ravi@authorcafe.com>
2022-09-28 17:14:47 +0530

    test code

--- committerName.sh
+++ committerName.sh
@@ -2,25 +2,26 @@
 
 commit_id=$(cat commit.txt)
 committername=$(git show -s --format='%cn' $commit_id)
-
-# blocks="blocks=[
-#             [
-#                 'type': 'section',
-#                 'text': [
-#                     'type': 'mrkdwn',
-#                     'text': \"*$committername*\n $commit_id commit name\"
-#                 ]
-#             ]
-#         ]"
+committermsg=$(git show -s --format='%s' $commit_id)
 
 blocks="[
-		{
-			\"type\": \"section\",
-			\"text\": {
-				\"type\": \"mrkdwn\",
-				\"text\": \"*$committername*\n $commit_id commit name\"
-			}
-		}
-	]"
+            [
+                'type': 'section',
+                'text': [
+                    'type': 'mrkdwn',
+                    'text': \"*$committername*\n $commit_id\n $committermsg\"
+                ]
+            ]
+        ]"
+
+# blocks="[
+# 		{
+# 			\"type\": \"section\",
+# 			\"text\": {
+# 				\"type\": \"mrkdwn\",
+# 				\"text\": \"*$committername*\n $commit_id commit name\"
+# 			}
+# 		}
+# 	]"
 
 echo $blocks
