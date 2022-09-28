e57d8548b50537ee6f14d533bc120d9500044c43
Ravi Teja Natchireddi <ravi@authorcafe.com>
2022-09-28 11:18:22 +0530

    test code

--- committerName.sh
+++ committerName.sh
@@ -3,14 +3,14 @@
 commit_id=$(cat commit.txt)
 committername=$(git show -s --format='%cn' $commit_id)
 
-blocks='[
+blocks="blocks=[
             [
-                "type": "section",
-                "text": [
-                    "type": "mrkdwn",
-                    "text": "*$committername*\n`$commit_id` commit name"
+                'type': 'section',
+                'text': [
+                    'type': 'mrkdwn',
+                    'text': \"*$committername*\n`$commit_id` commit name\"
                 ]
             ]
-        ]'
+        ]"
 
 echo $blocks
