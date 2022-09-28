8964475cefb510838655a9f4222aa438d5d560fe
sanjayvinai <35143545+sanjayvinai@users.noreply.github.com>
2022-09-28 16:49:40 +0530

    sanjay's commit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">HI</header>
+      <h1>sanjay's contribution for ravi's test on commit msgs</h1>
     </div>
   );
 }
